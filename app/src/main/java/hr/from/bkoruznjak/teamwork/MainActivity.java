0fbabdbd9760082eeeb8f8cc542d3cd4c0be40ea
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-09-27 09:03:50 +0200

    - setup android data binding

--- app/src/main/java/hr/from/bkoruznjak/teamwork/MainActivity.java
+++ app/src/main/java/hr/from/bkoruznjak/teamwork/MainActivity.java
@@ -1,13 +1,18 @@
 package hr.from.bkoruznjak.teamwork;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import hr.from.bkoruznjak.teamwork.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
